65b6480e0a5cb88cc34c2818c88b461c4bd50a74
Dave <dchukwudile@yahoo.com>
2022-11-15 11:30:31 +0100

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper/swiper-bundle.esm.js';
+    import 'swiper/swiper-bundle.css';
+    var swiper = new Swiper('.swiper', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
